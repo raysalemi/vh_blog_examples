acc28dbe5bbc5063629c95a0e1f397c5b47d1914
Ray Salemi <ray_salemi@mentor.com>
2021-08-16 11:10:15 -0700

    sensor sv

--- sensor/sensor.sv
+++ sensor/sensor.sv
@@ -4,31 +4,33 @@ class data;
    constraint example {sample >=50; sample <=80;}
 endclass 
 
-module sensor_reader();
+program sensor_reader();
     int samples[0:9];
 
     task data_from_sensor(output int sample);
         data datum;
         datum = new();
-        datum.randomize()
+        datum.randomize();
         #1;
         sample = datum.sample;
-        $display(sample)
+        $display(sample);
     endtask
 
     task gather_ten_samples();
         int unsigned sample;
         for (int ii = 0; ii < 10; ii++) begin
             data_from_sensor(sample);
-            samples[ii] = datum.sample
+            samples[ii] = sample;
         end
         $write("[ ");
-        for (ii = 0; ii < 10; ii++) begin
+        for (int ii = 0; ii < 10; ii++) begin
             $write("%0d, ", samples[ii]);
         end
         $write("]");
+        $display("");
+        $finish();
     endtask
 
     initial
         gather_ten_samples();
-
+endprogram
