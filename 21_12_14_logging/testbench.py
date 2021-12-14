c228cd14f07c5297b1ce55cccaaa27f1ea90cb26
Ray Salemi <ray_salemi@mentor.com>
2021-12-14 06:28:16 -0800

    fix flake issues

--- 21_12_14_logging/testbench.py
+++ 21_12_14_logging/testbench.py
@@ -3,19 +3,21 @@ from pyuvm import *
 import logging
 
 
-
 class WarningComp(uvm_component):
 
     def start_of_simulation_phase(self):
         self.logger.warning("This is WARNING")
 
+
 class InfoComp(uvm_component):
     def start_of_simulation_phase(self):
         self.logger.info("This is INFO")
 
+
 class DebugComp(uvm_component):
     def start_of_simulation_phase(self):
         self.logger.debug("This is DEBUG")
+
 
 class LogTest(uvm_test):
     def build_phase(self):
@@ -27,6 +29,7 @@ class LogTest(uvm_test):
         self.raise_objection()
         self.drop_objection()
 
+
 class DebugTest(LogTest):
     def end_of_elaboration_phase(self):
         self.set_logging_level_hier(DEBUG)
@@ -37,6 +40,7 @@ class FileTest(LogTest):
         file_handler = logging.FileHandler("log.txt", mode="w")
         self.add_logging_handler_hier(file_handler)
         self.remove_streaming_handler_hier()
+
 
 class NoLog(LogTest):
     def end_of_elaboration_phase(self):
@@ -54,6 +58,7 @@ async def debug_default_logging_level(_):
     """Demonstrate debug logging level"""
     uvm_report_object.set_default_logging_level(DEBUG)
     await uvm_root().run_test("LogTest")
+
 
 @cocotb.test()
 async def debug_logging_level(_):
